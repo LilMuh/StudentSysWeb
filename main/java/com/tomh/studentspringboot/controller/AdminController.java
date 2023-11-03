83372224aaf5d29d19483a953966969c69688408
Tom Huang <99058458+LilMuh@users.noreply.github.com>
2023-11-03 02:02:52 -0400

    Add files via upload

---
+++ main/java/com/tomh/studentspringboot/controller/AdminController.java
@@ -0,0 +1,54 @@
+package com.tomh.studentspringboot.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.tomh.studentspringboot.common.Result;
+import com.tomh.studentspringboot.entity.Admin;
+import com.tomh.studentspringboot.entity.Params;
+import com.tomh.studentspringboot.service.AdminService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Resource
+    private AdminService adminService;
+
+    @PostMapping
+    public Result save(@RequestBody Admin admin){
+        if(admin.getId()==0){
+            adminService.add(admin);
+        }else{
+            adminService.update(admin);
+        }
+        return Result.success();
+    }
+
+//    @PostMapping
+//    public Result login(@RequestBody Admin admin){
+//        Admin userLogin = adminService.login(admin);
+//        return Result.success();
+//    }
+
+    @GetMapping("/getAdmin")
+    public Result getAdmin(){
+        List<Admin> list = adminService.getAdmin();
+        return Result.success(list);
+    }
+
+    @GetMapping("/search")
+    public Result search(Params params){
+        PageInfo<Admin> info =  adminService.search(params);
+        return Result.success(info);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable int id){
+        adminService.delete(id);
+        return Result.success();
+    }
+}
---
+++ main/java/com/tomh/studentspringboot/entity/Params.java
@@ -0,0 +1,50 @@
+package com.tomh.studentspringboot.entity;
+
+public class Params {
+    private String username;
+    private String workerID;
+    private String phone;
+    private int pageNum;
+    private int pageSize;
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getWorkerID() {
+        return workerID;
+    }
+
+    public void setWorkerID(String workerID) {
+        this.workerID = workerID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
