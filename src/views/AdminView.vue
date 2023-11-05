8a01c01b47e1d82ef3a4884ada9f40dde8172159
Tom Huang <99058458+LilMuh@users.noreply.github.com>
2023-11-05 01:36:50 -0400

    Add files via upload

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -59,7 +59,29 @@
       <el-dialog title="Account information" :visible.sync="dialogFormVisible" width="40%">
         <el-form :model="form">
           <el-form-item label="Username" label-width="30%">
-            <el-input v-popover:username placeholder="abc123" v-model="form.username" autocomplete="off" style=" width:55%"></el-input>
+            <div>
+              <div class="input-container">
+                <el-input
+                    v-popover:username
+                    placeholder="abc123"
+                    v-model="form.username"
+                    autocomplete="off"
+                    @input="checkUsername"
+                    style="width: 55%"
+                    class="input-field"
+                ></el-input>
+                <div class="validation-icons">
+                  <i
+                      class="validation-icon-check el-icon-circle-check"
+                      v-show="form.username && validate.isUsernameValid"
+                  ></i>
+                  <i
+                      class="validation-icon-cross el-icon-circle-close"
+                      v-show="form.username && !validate.isUsernameValid"
+                  ></i>
+                </div>
+              </div>
+            </div>
           </el-form-item>
           <el-popover
               ref="username"
@@ -76,7 +98,25 @@
             </div>
           </el-popover>
           <el-form-item label="Password" label-width="30%">
-            <el-input v-popover:password placeholder="Abc12345." v-model="form.password" autocomplete="off" style=" width:55%"></el-input>
+            <div>
+              <div class="input-container">
+                <el-input class="input-field" @input="checkPassword" v-popover:password placeholder="Abc12345."
+                          v-model="form.password" autocomplete="off"
+                          style=" width:55%"></el-input>
+                <div class="validation-icons">
+                  <i
+                      class="validation-icon-check"
+                      v-if="form.password && validate.isPasswordValid"
+                      :class="'el-icon-circle-check'"
+                  ></i>
+                  <i
+                      class="validation-icon-cross"
+                      v-if="form.password && !validate.isPasswordValid"
+                      :class="'el-icon-circle-close'"
+                  ></i>
+                </div>
+              </div>
+            </div>
           </el-form-item>
           <el-popover
               ref="password"
@@ -105,7 +145,27 @@
             </div>
           </el-popover>
           <el-form-item label="Worker ID" label-width="30%">
-            <el-input v-popover:workerID placeholder="111111000011110010" v-model="form.workerID" autocomplete="off" style=" width:55%"></el-input>
+            <div>
+              <div class="input-container">
+                <el-input class="input-field" @input="checkWorkerID" v-popover:workerID placeholder="456693011021941121"
+                          v-model="form.workerID"
+                          autocomplete="off"
+                          style=" width:55%">
+                </el-input>
+                <div class="validation-icons">
+                  <i
+                      class="validation-icon-check"
+                      v-if="form.workerID && validate.isWorkerIDValid"
+                      :class="'el-icon-circle-check'"
+                  ></i>
+                  <i
+                      class="validation-icon-cross"
+                      v-if="form.workerID && !validate.isWorkerIDValid"
+                      :class="'el-icon-circle-close'"
+                  ></i>
+                </div>
+              </div>
+            </div>
           </el-form-item>
           <el-popover
               ref="workerID"
@@ -125,7 +185,26 @@
             </div>
           </el-popover>
           <el-form-item label="Phone number" label-width="30%">
-            <el-input v-popover:phone placeholder="4168886666" v-model="form.phoneNumber" autocomplete="off" style=" width:55%"></el-input>
+            <div>
+              <div class="input-container">
+                <el-input class="input-field" @input="checkPhone" v-popover:phone placeholder="2894802860"
+                          v-model="form.phoneNumber"
+                          autocomplete="off"
+                          style=" width:55%"></el-input>
+                <div class="validation-icons">
+                  <i
+                      class="validation-icon-check"
+                      v-if="form.phoneNumber && validate.isPhoneValid"
+                      :class="'el-icon-circle-check'"
+                  ></i>
+                  <i
+                      class="validation-icon-cross"
+                      v-if="form.phoneNumber && !validate.isPhoneValid"
+                      :class="'el-icon-circle-close'"
+                  ></i>
+                </div>
+              </div>
+            </div>
           </el-form-item>
           <el-popover
               ref="phone"
@@ -146,8 +225,8 @@
           </el-popover>
         </el-form>
         <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="submit()">Confirm</el-button>
+          <el-button @click="cancel">Cancel</el-button>
+          <el-button class="submit" type="primary" :disabled="isSubmitDisabled" @click="submit()">Confirm</el-button>
         </div>
       </el-dialog>
     </div>
@@ -169,10 +248,18 @@ export default {
         pageNum: 1,
         pageSize: 10,
       },
+      validate: {
+        isUsernameValid: false,
+        isPasswordValid: false,
+        isConfirmPasswordValid: false,
+        isWorkerIDValid: false,
+        isPhoneValid: false,
+      },
       tableData: [],
       total: 0,
       dialogFormVisible: false,
-      form: {}
+      form: {},
+      isSubmitDisabled: true,
     }
   },
   // do something at page creation
@@ -207,8 +294,13 @@ export default {
       this.dialogFormVisible = true;
     },
     edit(accountInfo) {
+      this.checkUsernameModify(accountInfo.username);
+      this.checkPasswordModify(accountInfo.password);
+      this.checkWorkerIDModify(accountInfo.workerID);
+      this.checkPhoneModify(accountInfo.phoneNumber);
       this.form = accountInfo;
       this.dialogFormVisible = true
+      this.search()
     },
     handleCurrentChange(pageNum) {
       this.params.pageNum = pageNum;
@@ -230,6 +322,10 @@ export default {
         }
       })
     },
+    cancel(){
+      this.dialogFormVisible = false;
+      this.search()
+    },
     submit() {
       request.post("/admin", this.form).then(res => {
         if (res.code === '0') {
@@ -246,10 +342,201 @@ export default {
           });
         }
       })
-    }
+    },
+    checkUsername() {
+      const username = this.form.username;
 
+      // Check if the username's length is within 3 to 15 characters
+      const isLengthValid = username.length >= 3 && username.length <= 15;
+
+      // Check if the username contains only alphanumeric characters
+      const isAlphanumeric = /^[a-zA-Z0-9]+$/.test(username);
+
+      // Check if there is at least one letter in the username
+      const hasLetter = /[a-zA-Z]/.test(username);
+
+      // Check all conditions
+      this.validate.isUsernameValid = isLengthValid && isAlphanumeric && hasLetter;
+      this.checkReadyRegistration();
+    },
+    checkPassword() {
+      const password = this.form.password;
+
+      // Check if the password has at least one uppercase letter
+      const hasUppercase = /[A-Z]/.test(password);
+
+      // Check if the password has at least one lowercase letter
+      const hasLowercase = /[a-z]/.test(password);
+
+      // Check if the password has at least one number
+      const hasNumber = /[0-9]/.test(password);
+
+      // Check if the password is longer than 8 characters
+      const isLengthValid = password.length > 8;
+
+      // Check if the password has at least one special symbol (, . !)
+      const hasSpecialSymbol = /[,.!@#$%^&*_+=`~/?<>;':]/.test(password);
+
+      // Check all conditions
+      this.validate.isPasswordValid =
+          hasUppercase && hasLowercase && hasNumber && isLengthValid && hasSpecialSymbol;
+      this.checkReadyRegistration();
+    },
+    checkWorkerID() {
+      const workerID = this.form.workerID;
+
+      // Check if the workerID is of length 18
+      const isLengthValid = workerID.length === 18;
+
+      // Check if the workerID does not start with 0
+      const doesNotStartWithZero = workerID[0] !== "0";
+
+      // Check if the first 17 characters are pure numbers
+      const hasPureNumbers = /^[0-9]{17}$/.test(workerID.substring(0, 17));
+
+      // Check if the last character is either a number, lowercase x, or uppercase X
+      const isLastCharValid = /[0-9xX]/.test(workerID[17]);
+
+      // Check all conditions
+      this.validate.isWorkerIDValid =
+          isLengthValid &&
+          doesNotStartWithZero &&
+          hasPureNumbers &&
+          isLastCharValid;
+      this.checkReadyRegistration();
+    },
+    checkPhone() {
+      const phoneNumber = this.form.phoneNumber;
+
+      // Check if the phoneNumber number is of length 10
+      const isLengthValid = phoneNumber.length === 10;
+
+      // Check if the phoneNumber number does not start with 0
+      const doesNotStartWithZero = phoneNumber[0] !== "0";
+
+      // Check if the phoneNumber number consists of pure numbers
+      const hasPureNumbers = /^[0-9]{10}$/.test(phoneNumber);
+
+      // Check all conditions
+      this.validate.isPhoneValid =
+          isLengthValid && doesNotStartWithZero && hasPureNumbers;
+      this.checkReadyRegistration();
+    },
+    checkReadyRegistration() {
+      // Check if all fields meet the required format criteria
+      this.isSubmitDisabled =
+          !this.validate.isUsernameValid ||
+          !this.validate.isPasswordValid ||
+          !this.validate.isWorkerIDValid ||
+          !this.validate.isPhoneValid;
+    },
+    // Modify
+    checkUsernameModify(username) {
+      // Check if the username's length is within 3 to 15 characters
+      const isLengthValid = username.length >= 3 && username.length <= 15;
+
+      // Check if the username contains only alphanumeric characters
+      const isAlphanumeric = /^[a-zA-Z0-9]+$/.test(username);
+
+      // Check if there is at least one letter in the username
+      const hasLetter = /[a-zA-Z]/.test(username);
+
+      // Check all conditions
+      this.validate.isUsernameValid = isLengthValid && isAlphanumeric && hasLetter;
+      this.checkReadyRegistration();
+    },
+    checkPasswordModify(password) {
+      // Check if the password has at least one uppercase letter
+      const hasUppercase = /[A-Z]/.test(password);
+
+      // Check if the password has at least one lowercase letter
+      const hasLowercase = /[a-z]/.test(password);
+
+      // Check if the password has at least one number
+      const hasNumber = /[0-9]/.test(password);
+
+      // Check if the password is longer than 8 characters
+      const isLengthValid = password.length > 8;
+
+      // Check if the password has at least one special symbol (, . !)
+      const hasSpecialSymbol = /[,.!@#$%^&*_+=`~/?<>;':]/.test(password);
+
+      // Check all conditions
+      this.validate.isPasswordValid =
+          hasUppercase && hasLowercase && hasNumber && isLengthValid && hasSpecialSymbol;
+      this.checkReadyRegistration();
+    },
+    checkWorkerIDModify(workerID) {
+      // Check if the workerID is of length 18
+      const isLengthValid = workerID.length === 18;
+
+      // Check if the workerID does not start with 0
+      const doesNotStartWithZero = workerID[0] !== "0";
+
+      // Check if the first 17 characters are pure numbers
+      const hasPureNumbers = /^[0-9]{17}$/.test(workerID.substring(0, 17));
+
+      // Check if the last character is either a number, lowercase x, or uppercase X
+      const isLastCharValid = /[0-9xX]/.test(workerID[17]);
+
+      // Check all conditions
+      this.validate.isWorkerIDValid =
+          isLengthValid &&
+          doesNotStartWithZero &&
+          hasPureNumbers &&
+          isLastCharValid;
+      this.checkReadyRegistration();
+    },
+    checkPhoneModify(phoneNumber) {
+      // Check if the phoneNumber number is of length 10
+      const isLengthValid = phoneNumber.length === 10;
+
+      // Check if the phoneNumber number does not start with 0
+      const doesNotStartWithZero = phoneNumber[0] !== "0";
+
+      // Check if the phoneNumber number consists of pure numbers
+      const hasPureNumbers = /^[0-9]{10}$/.test(phoneNumber);
+
+      // Check all conditions
+      this.validate.isPhoneValid =
+          isLengthValid && doesNotStartWithZero && hasPureNumbers;
+      this.checkReadyRegistration();
+    },
   }
 
 }
 </script>
+
+<style>
+.input-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.validation-icons {
+  position: absolute;
+  transform: translateY(-50%);
+  left: 255px; /* Adjust the left position as needed */
+  top: 18px;
+}
+
+.input-field {
+  width: 55%;
+}
+
+.validation-icon-cross,
+.validation-icon-check {
+  font-size: 18px;
+}
+
+.validation-icon-cross {
+  color: #de3131;
+}
+
+.validation-icon-check {
+  color: #5dd938;
+}
+</style>
+
 
